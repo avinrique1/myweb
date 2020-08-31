1e58486be387c4581f7e364fcfa07bed18c1fe94
avinrique1 <70524514+avinrique1@users.noreply.github.com>
2020-08-31 19:21:45 +0545

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,23 @@
+function search(){
+   var a = document.getElementById('hello').value
+    if(a==="home"){
+        window.open("http://127.0.0.1:5500/nav.html","_self")
+    }
+    if(a==="client"){
+        window.open("http://127.0.0.1:5500/client.html","_self")
+    }
+    if(a==="server"){
+        window.open("http://127.0.0.1:5500/server.html","_self")
+    }
+    if(a==="help"){
+        window.open("http://127.0.0.1:5500/help.html","_self")
+    }
+    if(a==="about"){
+        window.open("http://127.0.0.1:5500/about.html","_self")
+    }
+ 
+
+}
+function navmo(){
+
+}
